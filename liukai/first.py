e2c9ce7bb9d90d7dfdd88fce0f0a6efcf233ebf6
liukai <154824679@qq.com>
2018-03-16 00:37:15 +0800

    one day

---
+++ liukai/first.py
@@ -0,0 +1,59 @@
+print(5**2)
+cn = '🇨🇳'
+print(cn)
+cn_density = 2
+cn_people = 13
+print(cn_people//cn_density)
+rain_fall = 4.445e2
+print(rain_fall)
+cn_city, cn_town = 2, 3
+print(cn_city)
+print(cn_town)
+print(2)
+bool_true =  3 > 2
+bool_false = 3 < 2
+print(bool_true)
+print(bool_false)
+print('💍')
+diamond_ring = '钻戒💍'
+nack_lace = '项链'
+head_gear = diamond_ring + nack_lace
+print(head_gear)
+teacher_li = 'li'
+teacher_zhu = 'zhu'
+teachers = teacher_li + " & " + teacher_zhu
+print(teachers)
+salesman = '"I think you\'re an encyclopaedia salesman"'
+print(salesman)
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+# TODO: print a log message incorporating the strings above.
+# The message should be use the same format as this one:
+# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+message = username + ' accessed the site ' + url + ' at ' + timestamp + '.'
+print(message)
+print(type(133))
+print(type('string'))
+print(type(43.4))
+test = "hippo_"*2;
+print(test)
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+
+#TODO: Print a string with this format: This week's total sales: xxx
+# You will probably need to write several lines of code before the print statement.
+total = int(mon_sales) + int(tues_sales) + int(wed_sales) + int(wed_sales) + int(thurs_sales) + int(fri_sales)
+message = 'This week\'s total sales:' + str(total)
+print(message)
+#todo  11.字符串方法1
+
+
+
+
+
+
